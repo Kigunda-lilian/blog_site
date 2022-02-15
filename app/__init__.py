9211d0956d4edfdb56910f1856b9df2822cbd826
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-02-15 22:19:30 +0300

    add templates

--- app/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-
-#create an instance of my app
-app= Flask(__name__)
---
+++ app/main/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+#create an instance of my app
+app= Flask(__name__)
+
+#create auth instance
+
+from main import views
+
